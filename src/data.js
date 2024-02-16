57567bc9fc3f0e8c2320d342f93942baec736916
chizzy192 <122726814+chizzy192@users.noreply.github.com>
2024-02-16 16:11:53 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -11,9 +11,69 @@ const data = [
     },
     {
         "id": 3,
-        "title": "Scademin",
-        "description": "Refers to a teenage boy, usually a weeb who is at the same time autistic and diagnosed with cancer."
+        "title": "NGL",
+        "description": "Not Gonna Lie.",
+    },
+    {
+        "id": 4,
+        "title": "ISTG",
+        "description": "I Swear To God."
+    },
+    {
+        "id": 5,
+        "title": "FR",
+        "description": "For Real."
     }
+,{
+    "id": 6,
+    "title": "ASAP",
+    "description": "As Soon As Possible."
+},
+{
+    "id": 7,
+    "title": "BRB",
+    "description": "Be Right Back."
+},
+{
+    "id": 8,
+    "title": "EOBD",
+    "description": "End Of Business Day."
+},
+{
+    "id": 9,
+    "title": "WCW",
+    "description": "Women Crush Wednesday."
+},
+{
+    "id": 10,
+    "title": "BYOB",
+    "description": "Bring Your Own Beer/Beverage."
+},
+{
+    "id": 11,
+    "title": "IKR",
+    "description": "I Know Right."
+},
+{
+    "id": 12,
+    "title": "ATM",
+    "description": "At this moment."
+},
+{
+    "id": 13,
+    "title": "ATP",
+    "description": "At this Point."
+},
+{
+    "id": 14,
+    "title": "OOMF",
+    "description": "One Of My Friends."
+},
+{
+    "id": 15,
+    "title": "TTYL",
+    "description": "Talk To You Later."
+}
 ]
 
 export default data
